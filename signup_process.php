48894040350935728001c73c3efa6f02fe9bebf1
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-07 22:01:34 +0800

    update here

---
+++ signup_process.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require_once 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $role = mysqli_real_escape_string($conn, $_POST['role']);
+
+    // Check if the username is already taken
+    $check_query = "SELECT * FROM users WHERE username='$username'";
+    $check_result = mysqli_query($conn, $check_query);
+
+    if ($check_result && mysqli_num_rows($check_result) > 0) {
+        echo "Username already exists. Please choose a different username.";
+    } else {
+        // Insert the new user into the database
+        $insert_query = "INSERT INTO users (username, password, role) VALUES ('$username', '$password', '$role')";
+        $insert_result = mysqli_query($conn, $insert_query);
+
+        if ($insert_result) {
+            // Display an alert on successful registration
+            echo '<script>alert("Registered successfully. You can now log in.");</script>';
+            // You can redirect the user to the login page if needed.
+        } else {
+            echo "Error in registration. Please try again.";
+        }
+    }
+}
+?>
