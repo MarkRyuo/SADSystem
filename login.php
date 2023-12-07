d182e96e2842908733688b414e4b238e5e688c74
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-08 07:53:17 +0800

    add

--- login.php
+++ login.php
@@ -21,6 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             header("Location: student_dashboard.php");
         }
     } else {
+        header("Location: index.html");
         echo "Invalid credentials.";
     }
 }
